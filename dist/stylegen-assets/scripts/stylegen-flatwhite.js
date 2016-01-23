253f4f7b02c0c3a8c2158960fbc0257afd2cb793
Falk Hoppe <falkhoppe81@gmail.com>
2016-01-23 10:04:20 +0100

    resizable iframes with auto-height lookouts

--- dist/stylegen-assets/scripts/stylegen-flatwhite.js
+++ dist/stylegen-assets/scripts/stylegen-flatwhite.js
@@ -13,4 +13,38 @@
     navItem.addClass('active');
     tabs.find(link.attr('href')).addClass('active')
   });
+
+
+  var resizeIFrames = function() {
+    // we memoize our iframes here (so if anyone in the far future wants to add components async, this has to be adjusted)
+    var iframes = $('iframe.auto-height');
+
+    $.each(iframes, function(i, iframe) {
+      var $iframeBody, $iframe, resize;
+      $iframe = $(iframe);
+
+      resize = function() {
+        var scrollHeight;
+
+        if ($iframeBody) {
+          scrollHeight = $iframeBody.height() + 'px';
+          if (i === 0) { console.log(scrollHeight); }
+          $iframe.height(scrollHeight);
+          setTimeout(resize, 500);
+        } else {
+          $iframe.load(function() {
+            $iframeBody = $(iframe.contentWindow.document.body);
+            $iframe.height($iframeBody.height() + 'px');
+            setTimeout(resize, 500);
+          });
+        }
+      };
+
+      resize();
+    });
+  };
+
+  resizeIFrames();
+
+
 }(window && window.jQuery))
